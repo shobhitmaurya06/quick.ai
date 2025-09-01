ed857e5fbf11be523184c655727fdf0ee36bf296
shobhitmaurya06 <shobhitmaurya9346@gmail.com>
2025-09-01 08:17:16 +0530

    write article api completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import DashBoard from "./pages/DashBoard";
@@ -10,9 +10,12 @@ import ReviewResume from "./pages/ReviewResume";
 import { GenerateImages } from "./pages/GenerateImages";
 import BlogTitle from "./pages/BlogTitle";
 import Home from "./pages/Home";
+import { useAuth } from "@clerk/clerk-react";
+import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
     <div>
+      <Toaster/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/ai" element={<Layout />}>
--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -1,16 +1,41 @@
 import { Edit, Sparkles } from "lucide-react";
 import React, { useState } from "react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+import Markdown from "react-markdown";
 const WriteArticle = () => {
   const articleLength = [
     { length: 800, text: "Short (500-800 words)" },
     { length: 1200, text: "Medium (800-1200 words)" },
     { length: 1600, text: "Long (1200-1600 words)" },
   ];
-  const submitHandler=async(e)=>{
-    e.preventDefault();
-  }
+  axios.defaults.baseURL=import.meta.env.VITE_BASE_URL
   const [selectedLength,setSelectedlength]=useState(articleLength[0]);
+  const [loading,setLoading]=useState(false);
+  const [content,setContent]=useState('');
+  const {getToken}=useAuth();
   const [input ,setInput]=useState("");
+   const submitHandler=async(e)=>{
+    e.preventDefault();
+    try{
+      setLoading(true);
+      const prompt=`write an article about ${input} in ${selectedLength.text}`;
+      const {data}=await axios.post('/api/ai/generate-article',{prompt,length:selectedLength.length},{
+        headers:{Authorization:`Bearer ${await getToken()}`}
+      })
+      if(data.success){
+        setContent(data.content);
+      }
+      else{
+        toast.error(data.message);
+      }
+    }catch(error){
+       toast.error(error.message);
+
+    }
+    setLoading(false);
+  }
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
       <form onSubmit={submitHandler}  className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200">
@@ -39,8 +64,10 @@ const WriteArticle = () => {
 ))}
 </div>
 <br />
-<button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer ">
-  <Edit  className="w-5"/>
+<button disabled={loading} className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer ">
+  {
+    loading ?<span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>:<Edit  className="w-5"/>
+  }
     Generate Article
 </button>
         
@@ -49,16 +76,19 @@ const WriteArticle = () => {
       <div className="w-full max-w-lg bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
         <div className="flex items-center gap-3">
           <Edit className="w-5 h-5 text-[#4A7AFF]"/>
-          <h1 className="text-xl font-semibold">Article Configuration</h1>
+          <h1 className="text-xl font-semibold">Generated Article</h1>
         </div>
-        <div className="flex-1 flex justify-center items-center ">
+        {!content ?(  <div className="flex-1 flex justify-center items-center ">
          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
           <Edit className="w-9 h-9"/>
           <p className="tetx-sm">Enter a topic and click "Generate article " to get started</p>
           </div> 
-        </div>
-
-
+        </div>):(
+          <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+            <div className="reset-tw"><Markdown>{content}</Markdown></div>
+          </div>
+        )}
+      
       </div>
     </div>
   );
